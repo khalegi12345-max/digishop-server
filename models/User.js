e785ea3e376f9446712731a1c9eb28db0b80f8e5
khalegi12345-max <khalegi12345@gmail.com>
2026-05-07 15:35:39 +0300

    fix user nodel

--- models/User.js
+++ models/User.js
@@ -1,49 +1,23 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const UserSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
   },
-  { timestamps: true },
-);
-
-module.exports = mongoose.model("User", UserSchema);
-
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-    },
+  email: {
+    type: String,
+    required: true,
+    unique: true
   },
-  { timestamps: true },
-);
+  password: {
+    type: String,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true })
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema)
